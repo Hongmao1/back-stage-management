409b51d470e27e5abd24265cf62279186c4a2282
d8881r <13620351032@163.com>
2022-03-26 18:55:23 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home_container">
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .home_container {
+    background-color: pink;
+  }
+</style>
